7070eed0008116dc0cb785f4a58c1a7c994eec05
Xiaohan0416 <dianaxu579@gmail.com>
2024-02-01 15:39:38 +1000

    Complete jobscript of SRR14800535 sequence data

--- test/jobs/SRR14800535_cellranger.sh
+++ test/jobs/SRR14800535_cellranger.sh
@@ -19,9 +19,9 @@ mkdir $TMPDIR/fastq
 mkdir $TMPDIR/fastq/SRR14800535
 
 # make the meta.csv files for running cellranger
-# python /scratch/user/s4543064/Xiaohan_Summer_Research/test/scripts/make_meta.py --folder $TMPDIR --sample SRR14800535
-cp /path/to/SRR14800535_1.fastq /path/to/SRR14800535_S1_L001_R1_001.fastq 
-cp /path/to/SRR14800535_2.fastq /path/to/SRR14800535_S1_L001_R2_001.fastq 
+python /scratch/user/s4543064/Xiaohan_Summer_Research/test/make_meta.py --folder $TMPDIR --sample SRR14800535
+cp /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_1.fastq /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R1_001.fastq 
+cp /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_2.fastq /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R2_001.fastq 
 
 gzip /path/to/SRR14800535_S1_L001_R1_001.fastq 
 gzip /path/to/SRR14800535_S1_L001_R2_001.fastq 
