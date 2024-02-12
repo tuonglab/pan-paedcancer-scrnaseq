d6181039f44861f58e5281963858204e781e7df4
johnNoxas <john.saxon02@outlook.com>
2024-02-12 15:14:13 +1000

    geo_get.sh script to import and unpack files from GEO

---
+++ scripts/geo_get.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Usage: bash scripts/geo_get.sh GSE123456
+
+accession_code=$1
+
+wget -e robots=off -nH -nd \
+    "ftp://ftp.ncbi.nlm.nih.gov/geo/series/${accession_code:0:${#accession_code}-3}nnn/${accession_code}/suppl/${accession_code}_RAW.tar" \
+    -P data
+
+mkdir "data/${accession_code}_RAW/"
+
+tar -xvf  data/${accession_code}_RAW.tar -C data/${accession_code}_RAW
+rm data/${accession_code}_RAW.tar
+
+tree -o trees/${accession_code}_tree.txt -J  data/${accession_code}_RAW
+
+exit
