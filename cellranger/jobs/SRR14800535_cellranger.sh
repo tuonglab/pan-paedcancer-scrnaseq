8836c013ffe7e9c43619fac6d00b9deeaa153da3
Xiaohan0416 <dianaxu579@gmail.com>
2024-02-21 09:52:59 +1000

    Try to fix the "samples not ofund" error by deleting the --sample option

--- cellranger/jobs/SRR14800535_cellranger.sh
+++ cellranger/jobs/SRR14800535_cellranger.sh
@@ -20,21 +20,20 @@ mkdir $TMPDIR/fastq/SRR14800535
 
 # make the meta.csv files for running cellranger
 # python /scratch/user/s4543064/xiaohan-john-project/cellranger/make_meta.py --folder $TMPDIR --sample SRR14800535
-cp /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_1.fastq /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R1_001.fastq 
-echo 'I have created the SRR14800535_S1_L001_R1_001.fastq file'
-cp /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_2.fastq /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R2_001.fastq 
-echo 'I have created the SRR14800535_S1_L001_R2_001.fastq file'
+# cp /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_1.fastq /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R1_001.fastq 
+# echo 'I have created the SRR14800535_S1_L001_R1_001.fastq file'
+# cp /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_2.fastq /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R2_001.fastq 
+# echo 'I have created the SRR14800535_S1_L001_R2_001.fastq file'
 
-gzip /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R1_001.fastq  
-gzip /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R2_001.fastq  
+# gzip /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R1_001.fastq  
+# gzip /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_S1_L001_R2_001.fastq  
 
 rsync -azvhP --no-perms --no-owner --no-group /QRISdata/Q6104/Xiaohan/1_Datasets/PRJNA737188/SRR14800535_*.fastq.gz $TMPDIR/fastq/SRR14800535/
 # run cellranger
 cellranger count --id=SRR14800535 \
        --transcriptome=/home/s4543064/refdata-gex-GRCh38-2020-A \
        --fastqs=$TMPDIR/fastq/SRR14800535/ \
-       --sample=SRR14800535_L001 \
-       --localcores=64 \aria
+       --localcores=64 \
        --localmem=256
 
 rsync -azvhP --no-perms --no-owner --no-group $TMPDIR/SRR14800535 /QRISdata/Q6104/Xiaohan/3_cellranger_outputs
